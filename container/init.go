ffbe7a2d6edd20db4cdd5e0498abcd7b3237b2ed
Lyn <lyn1990828@gmail.com>
2019-10-17 11:26:55 +0800

    feat(container): add pipe and env var support when we pass user cmd
    attach pipe to init cmd, then we can start new parent process and wait for user command, before we
    send user command, we can get parent process pid, so we can add cgroup limit before pareent process
    exec user command, and if we use pipe, we can send some special characters. add env var support.

--- container/init.go
+++ container/init.go
@@ -1,22 +1,49 @@
 package container
 
 import (
+	"fmt"
 	"github.com/Sirupsen/logrus"
+	"io/ioutil"
 	"os"
+	"os/exec"
+	"strings"
 	"syscall"
 )
 
-func RunContainerInitProcess(command string, args []string) error {
-	logrus.Infof("container init command %s", command)
+func RunContainerInitProcess() error {
+	cmdArray := readUserCommand()
+	if nil == cmdArray || len(cmdArray) == 0 {
+		return fmt.Errorf("Run container get user command error, cmdArray is nil")
+	}
+	logrus.Infof("container user command [%s]", strings.Join(cmdArray, " "))
 
 	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
 	// mount proc, or `ps` will search parents proc
 	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+	// support env var
+	path, err := exec.LookPath(cmdArray[0])
+	if nil != err {
+		logrus.Errorf("Exec look path error %v", err)
+		return err
+	}
+	logrus.Infof("Find exec path: %s", path)
 
-	argv := []string{command}
 	// replace init proc with command
-	if err := syscall.Exec(command, argv, os.Environ()); nil != err {
+	if err := syscall.Exec(path, cmdArray[0:], os.Environ()); nil != err {
 		logrus.Errorf(err.Error())
 	}
 	return nil
+}
+
+// read user command from the pipe we created
+func readUserCommand() []string {
+	// 3 is read pipe (the fourth pipe is the the cmd extraFiles)
+	pipe := os.NewFile(uintptr(3), "pipe")
+	msg, err := ioutil.ReadAll(pipe)
+	if nil != err {
+		logrus.Errorf("init read pipe error %v", err)
+		return nil
+	}
+	msgStr := string(msg)
+	return strings.Split(msgStr, " ")
 }
--- run.go
+++ run.go
@@ -4,14 +4,27 @@ import (
 	"github.com/3i2bgod/mydocker/container"
 	"github.com/Sirupsen/logrus"
 	"os"
+	"strings"
 )
 
-func Run(tty bool, command string)  {
-	parent := container.NewParentProcess(tty, command)
+func Run(tty bool, cmdArray []string)  {
+	parent, writePipe := container.NewParentProcess(tty)
 
+	if nil == parent {
+		logrus.Error("new parent process error")
+		return
+	}
 	if err := parent.Start(); nil != err  {
 		logrus.Error(err)
 	}
+	sendInitCommand(cmdArray, writePipe)
 	parent.Wait()
-	os.Exit(-1)
+	os.Exit(0)
+}
+
+func sendInitCommand(cmdArray []string, writePipe *os.File) {
+	command := strings.Join(cmdArray, " ")
+	logrus.Infof("command all is [%s]", command)
+	writePipe.WriteString(command)
+	writePipe.Close()
 }
