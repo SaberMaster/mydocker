3e35cba939c016e60d682c8114d91c95f5767e99
Lyn <lyn1990828@gmail.com>
2020-04-12 22:18:14 +0800

    feat(all): add exec command
    use exec command to enter the container's namespace

---
+++ exec.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/3i2bgod/mydocker/container"
+	"github.com/Sirupsen/logrus"
+	"os"
+	"os/exec"
+	"strings"
+	// we must include this file manual, or this file will be skipped
+	_ "github.com/3i2bgod/mydocker/nsenter"
+)
+
+const ENV_EXEC_PID =  "mydocker_pid"
+const ENV_EXEC_CMD =  "mydocker_cmd"
+
+func ExecContainer(containerName string, cmdArray []string) {
+	pid, err := getContainerPidByName(containerName)
+
+	if nil != err {
+		logrus.Errorf("Exec container getContainerPidByName %s error %v", containerName, err)
+		return
+	}
+
+	cmdStr := strings.Join(cmdArray, " ")
+	logrus.Infof("container pid %s", pid)
+	logrus.Infof("command %s", cmdStr)
+
+	// run exec cmd in forked child process
+	cmd := exec.Command("/proc/self/exe", "exec")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	os.Setenv(ENV_EXEC_PID, pid)
+	os.Setenv(ENV_EXEC_CMD, cmdStr)
+
+	if err := cmd.Run(); nil != err {
+		logrus.Errorf("Exec container %s error %v", containerName, err)
+	}
+}
+
+func getContainerPidByName(containerName string) (string, error) {
+	containerInfo, err := container.GetContainerInfo(containerName)
+	if nil != err {
+		logrus.Errorf("Get container: %s err: %v", containerName, err)
+		return "", err
+	}
+
+	return containerInfo.Pid, nil
+}
---
+++ nsenter/nsenter.go
@@ -0,0 +1,59 @@
+package nsenter
+
+/*
+#include <errno.h>
+#include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+
+
+// this function will auto execute when this package be imported
+// will be executed when this process start
+__attribute__((constructor)) void enter_namespace(void) {
+	// get mydocker pid
+	char* mydocker_pid = getenv("mydocker_pid");
+	if (mydocker_pid) {
+		fprintf(stdout, "got mydocker_pid=%s\n", mydocker_pid);
+	} else {
+		fprintf(stdout, "missing mydocker_pid env skip nsenter\n");
+		return;
+	}
+
+	// get mydocker cmd
+	char* mydocker_cmd = getenv("mydocker_cmd");
+	if (mydocker_cmd) {
+		fprintf(stdout, "got mydocker_cmd=%s\n", mydocker_cmd);
+	} else {
+		fprintf(stdout, "missing mydocker_cmd env skip nsenter\n");
+		return;
+	}
+
+	int i;
+	char nspath[1024];
+	char* namespaces[] = {"ipc", "uts", "net", "pid", "mnt"};
+
+	// enter namespace
+	for (i = 0; i < 5; i++) {
+		// path example: /proc/pid/ns/ipc
+		sprintf(nspath, "/proc/%s/ns/%s", mydocker_pid, namespaces[i]);
+
+		int fd = open(nspath, O_RDONLY);
+
+		if (-1 == setns(fd, 0)) {
+			fprintf(stderr, "setns on %s namespace faileds: %s\n", namespaces[i], strerror(errno));
+		} else {
+			fprintf(stdout, "setns on %s namespace successed\n", namespaces[i]);
+		}
+		close(fd);
+	}
+
+	// exec cmd
+	int res = system(mydocker_cmd);
+	//exit
+	exit(0);
+	return;
+}
+ */
+import "C"
